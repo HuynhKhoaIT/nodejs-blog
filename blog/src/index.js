1a98d33abefd15ef6782691822005ac2db257d79
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2023-07-16 22:23:14 +0700

    cài husky

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,40 +1,40 @@
-
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded(
-  {
-    extended: true
-  }
-));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // XMLHttpRequest, fetch, axios,...
 
 // HTTP  logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // template engine
-app.engine('hbs', handlebars({
-  extname: ".hbs"
-}
-));
+            app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+                }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
 // Routers init
 route(app);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}/`)
-})
+    console.log(`Example app listening on port http://localhost:${port}/`);
+});
