ccdbb9491cbeca8be1f97afa854f81154ae8ada0
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2023-07-16 16:06:13 +0700

    [MVC] Routes & Controllers

--- blog/src/index.js
+++ blog/src/index.js
@@ -7,7 +7,18 @@ const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded(
+  {
+    extended: true
+  }
+));
+app.use(express.json());
+
+// XMLHttpRequest, fetch, axios,...
 
 // HTTP  logger
 app.use(morgan('combined'))
@@ -20,10 +31,9 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources\\views'));
 
-console.log("path:"+  path.join(__dirname, 'resources/views'))
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Routers init
+route(app);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}/`)
